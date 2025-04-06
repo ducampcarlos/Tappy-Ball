786bbf288bb411f3c429058e5a066b96629f560c
ducampcarlos <ducampcarlos@gmail.com>
2025-04-05 21:10:15 -0300

    Added: Player Jump.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public bool IsPressed { get; private set; }
+    Rigidbody rb;
+    [SerializeField] float jumpForce = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IsPressed = false;
+
+#if UNITY_ANDROID || UNITY_EDITOR
+        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.wasPressedThisFrame)
+        {
+            IsPressed = true;
+        }
+#endif
+#if UNITY_STANDALONE || UNITY_EDITOR
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            IsPressed = true;
+        }
+#endif
+
+        if (IsPressed)
+        {
+            rb.linearVelocity = Vector3.up * jumpForce;
+        }
     }
 }
